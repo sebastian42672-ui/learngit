da9c71efcbf1ca8e34ac1d8659cb271d34307741
Sebastian Scott Fry <sebastian42672@gmail.com>
2025-10-07 09:44:52 +0300

    Add infinite math and negative 0 examples

---
+++ script.js
@@ -0,0 +1,12 @@
+let inf = Infinity;
+inf = -Infinity;
+console.log(inf);
+inf = 0 / 5;
+inf = 5 / 0;
+inf = -5 / 0;
+console.log(inf);
+let num =3 * 0;
+console.log(num);
+num = -3 * 0;
+console.log(num);
+
