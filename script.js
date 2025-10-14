8677191007c8d1a4d01c0f6116936cfcbd778b03
Sebastian Scott Fry <sebastian42672@gmail.com>
2025-10-14 09:11:30 +0300

    else,if, else if switch case

--- script.js
+++ script.js
@@ -1,12 +1,48 @@
-let inf = Infinity;
-inf = -Infinity;
-console.log(inf);
-inf = 0 / 5;
-inf = 5 / 0;
-inf = -5 / 0;
-console.log(inf);
-let num =3 * 0;
-console.log(num);
-num = -3 * 0;
-console.log(num);
+let num = 5;
+if(num > 10) {
+    console.log(`Suuurem kui kümme!`);
+} else if (num=== 10) {
+    console.log(` võrdne kümnega!`);
+} else {
+    console.log(` Väiksemkui kümme!`);
+}
 
+last day = new Date(`2025-10-13`).get day();
+console.log(day);
+if(day === 1) {
+
+    console.log (`Esmaspäev`);
+} else if(day === 2) {
+        console.log (`Teisipäev`);
+} else if(day === 3) {
+        console.log (`Kolmapäev`);
+} else if(day === 4) {
+        console.log (`Neljapäev`);
+} else if(day === 5) {
+        console.log (`Reede`);
+} else if(day === 6) {
+        console.log (`Laupäev`);
+} else if(day === 0) {
+        console.log (`Pühapäev`);
+} else if(`imelikpäev`) {
+
+switch (day) {
+    case 1:
+        console.log(`esmaspäaev);
+            break;
+    case 2:
+        console.log(`Teisipäev);
+            break;
+    case 3:
+        console.log(`Kolmapäev);
+            break;
+    case 4:
+        console.log(`Neljapäaev);
+            break;
+    case 5:
+        console.log(`Reede);
+            break;
+    case 6:
+        console.log(`Laupäaev);
+            break;
+}
