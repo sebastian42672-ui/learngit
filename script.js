76db4f463c6406d2b13b44d4119982f7b3a0c18d
Sebastian Scott Fry <sebastian42672@gmail.com>
2025-10-14 09:39:52 +0300

    Add loop examples

--- script.js
+++ script.js
@@ -1,48 +1,43 @@
+ for(let i=0;i<10;i++){
+    console.log(i);
+}
+
+for(let i=9;i>0;1--) {
+    console.log(i);
+}
+
+for(let i=1;i<1_000_000;i*=2) {
+    console.log(i)
+}
+
+for(let i=0;i>10;i++){
+    console.log(i);
+}
+
+// for(let i=0;i<10;i--){
+//     console.log(i)
+// }
+
+let start = Date.now();
+console.log(start);
+let i =0;
+while(Date.now() < start+5000) {
+    i++;
+}
+console.log(i);
+
+// while(true) {
+
+// }
+
 let num = 5;
-if(num > 10) {
-    console.log(`Suuurem kui kümme!`);
-} else if (num=== 10) {
-    console.log(` võrdne kümnega!`);
-} else {
-    console.log(` Väiksemkui kümme!`);
+
+while(num>5){
+    console.log(`WHILW`);
 }
 
-last day = new Date(`2025-10-13`).get day();
-console.log(day);
-if(day === 1) {
+do {
+    console.log(`DO`);
+} while(num>5);
 
-    console.log (`Esmaspäev`);
-} else if(day === 2) {
-        console.log (`Teisipäev`);
-} else if(day === 3) {
-        console.log (`Kolmapäev`);
-} else if(day === 4) {
-        console.log (`Neljapäev`);
-} else if(day === 5) {
-        console.log (`Reede`);
-} else if(day === 6) {
-        console.log (`Laupäev`);
-} else if(day === 0) {
-        console.log (`Pühapäev`);
-} else if(`imelikpäev`) {
 
-switch (day) {
-    case 1:
-        console.log(`esmaspäaev);
-            break;
-    case 2:
-        console.log(`Teisipäev);
-            break;
-    case 3:
-        console.log(`Kolmapäev);
-            break;
-    case 4:
-        console.log(`Neljapäaev);
-            break;
-    case 5:
-        console.log(`Reede);
-            break;
-    case 6:
-        console.log(`Laupäaev);
-            break;
-}
