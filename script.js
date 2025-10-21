e5d10f5d98ab164b696f3378d3be38dd8ae4df3b
Sebastian Scott Fry <sebastian42672@gmail.com>
2025-10-21 09:45:39 +0300

    add spread and destrucuring texamples

--- script.js
+++ script.js
@@ -1,71 +1,33 @@
-function hello() {
-    console.log(`Hello World!`);
+let num = [1, 2, 3, 4];
+let moreNums = [5,6, ...nums, 7, 8];
+console.log(moreNums);
+
+function SubmitEvent(...args) {
+    console.log(args);
+    let sum = 0;
+    for(let i=0; i<args.length; i++) {
+        sum+=args[i];
+    }
+    return sum;
 }
 
-hello();
-hello();
-hello();
+sum(1,2);
+let a = sum(1,2,3,4,5,6,7,8,9);
+console.log(a);
+a = sum(...moreNums);
+console.log(a);
 
-function helloName(name=`Nameless`) {
-    console.loh(`Hello ${name}!`);
-}
-
-helloName(`Arvo`);
-helloName(`Arvi`);
-helloName(`Toomas`);
-helloName();
-
-function sum(a, b) {
-    return a+b;
-    console.log(`After return`); // never runs
-}
-
-let answer = sum(4,6);
-console.log(answer);
-
-let sum2 = function (a, b) {
-    return a+b;
-}
-console.log(sum2(3,5));
-
-let sum3 = (a,b) => {
-    return a+b;
-}
-
-console.log(sum3(2,3));
-
-let sum4 = (a,b) => a+b;
-
-console.log(sum4(6,4));
-
-let square = a => a*a;
-
-console.log(square(6));
-
-let obj = {
-    name: `Arvo` ,
+let person = {
+    name: `Arvo`
     age: 69,
-    sum1: function (a,b) [
-        return a+b+this.age;
-    ],
-    sum2: (a,b) => {
-        return a+b+this.age;
-    },
-    sum3: (a,b) =>
-    sum(a,b){
-        return a+b+this.age,
-    }
 }
 
-console.log(obj.sum1(1,1));
-console.log(obj.sum2(1,1));
-console.log(obj.sum3(1,1));
-console.log(obj.sum4(1,1));
+let {name,age} = person;
+console.log(name);
+console.log(log);
 
-function recursive(i){
-    if(i<10){
-        console.log(i);
-        recursive(i+1);
-    }
+function ageSquared({age}){
+    return age*age;
 }
-recursive(0);
+
+console.log(ageSquared(person));
