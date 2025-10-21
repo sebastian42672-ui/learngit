cedff32c63e7ba0f82d17aba938af36076335393
Sebastian Scott Fry <sebastian42672@gmail.com>
2025-10-21 09:56:56 +0300

    add array loop funktsion

--- script.js
+++ script.js
@@ -1,33 +1,9 @@
-let num = [1, 2, 3, 4];
-let moreNums = [5,6, ...nums, 7, 8];
-console.log(moreNums);
+let nums = [1,2,3,4,5,67,8,9];
 
-function SubmitEvent(...args) {
-    console.log(args);
-    let sum = 0;
-    for(let i=0; i<args.length; i++) {
-        sum+=args[i];
-    }
-    return sum;
-}
+nums.forEach(num => console.log(num));
+nums.forEach((num,i) =>console.log(num,i));
 
-sum(1,2);
-let a = sum(1,2,3,4,5,6,7,8,9);
-console.log(a);
-a = sum(...moreNums);
-console.log(a);
-
-let person = {
-    name: `Arvo`
-    age: 69,
-}
-
-let {name,age} = person;
-console.log(name);
-console.log(log);
-
-function ageSquared({age}){
-    return age*age;
-}
-
-console.log(ageSquared(person));
+let even = nums.filter(num => num%2 === 0);
+console.log(even);
+let sum = nums.reduce((sum, num) => sum+num);
+console.log(sum);
