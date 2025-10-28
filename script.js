b66c21319148e8089bcc138195a01fbd2ef77c46
Sebastian Scott Fry <sebastian42672@gmail.com>
2025-10-28 09:00:45 +0200

    timeout interval examples

--- script.js
+++ script.js
@@ -1,27 +1,22 @@
-let nums = [6,4,5,3,2,1,7,8];
+setTimeout(()=> {
+    console.log(`later`);
+}, 10_000);
 
-nums.forEach(num => console.log(num));
-nums.forEach((num,i) =>console.log(num,i));
+console.log(`now`);
 
-let even = nums.filter(num => num%2 === 0);
-console.log(even);
-let sum = nums.reduce((sum, num) => sum+num);
-console.log(sum);
+setInterval(() => {
+    console.log(`again`);
+}, 10_000);
 
-let sorted = nun.sort((a,b) => {
-    if (a > b) {
-        return 1;
-    }
-    if (a < b) {
-        return 1;
-    }
-    return 0;
+let answer = fetch(`http://127.0.0.1:5500/`). then(response => {
+    console.log(response.text);
+}).then(text => {
+    console.log(text);
 });
-console.log(sorted);
 
-let sorted2 = nums.sort((a,b) => a-b);
-console.log(sorted2);
+console.log(answer);
+let response = await fetch(`http://127.0.0.1:5500/`);
+console.log(response);
+let text = await response.text();
+console.log(text);
 
-let squares = nums.map(num => num+num)
-
-console.log(squares);
