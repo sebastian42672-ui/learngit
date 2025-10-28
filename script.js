0fa9aecb01639f523abb719655d7274d35d5a267
Sebastian Scott Fry <sebastian42672@gmail.com>
2025-10-28 09:58:21 +0200

    evendid

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
-setTimeout(()=> {
-    console.log(`later`);
-}, 10_000);
+console.log(window, {document});
+let heading = document.querySelector(`h1`)
+console.log({heading});
+heading.innertext = `Hello DOM!`;
 
-console.log(`now`);
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
 
-setInterval(() => {
-    console.log(`again`);
-}, 10_000);
-
-let answer = fetch(`http://127.0.0.1:5500/`). then(response => {
-    console.log(response.text);
-}).then(text => {
-    console.log(text);
+let button = document.querySelector(`button`);
+button.addEventListener(`click`, event => {
+    console.log(event);
+    heading .innertext = 'color';
+    heading.style.color = getRandomColor();
 });
+setInterval() => {
+        document.body.style.background = getRandomColor();
+}, 10_000);
 
-console.log(answer);
-let response = await fetch(`http://127.0.0.1:5500/`);
-console.log(response);
-let text = await response.text();
-console.log(text);
+let input = document.querySelector('input');
 
+input.addEventListener('change', event => {
+    console.log(event);
+    heading.innertext = input.value.split('').reverse().join('');
+});
